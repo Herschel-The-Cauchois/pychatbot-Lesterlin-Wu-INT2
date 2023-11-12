2443474cd8113c34b95ba4601c7fda6c734c9cc8
Herschel_TC <raphael.lesterlin@gmail.com>
2023-11-12 21:13:43 +0100

    Added comments to the code, for all functions, also forgot to use the error info list in the log, which will now be written as a result to the function in cleaner_log.txt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from elementary_functions import *
 
+"""The main.py file here is a small testing program cleaning all the speeches files downloaded, then checking the 
+cleaning errors in that treated output."""
 list1 = list_of_files("./speeches", ".txt")
 for elem in list1:
     file_cleaner("./speeches/" + elem)
